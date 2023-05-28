f8d17e18b7398f1f3a36ba4e790cbaaa15332128
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2023-05-29 00:36:20 +0100

    Add form to submit original URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'links#new'
+  post '/links/create', to: 'links#create', as: "create_link"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
